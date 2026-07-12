81c32a3600db5853bac62f8c0a8bf19a70ebf6ab
m_ahmoud <melngar650@gmail.com>
2026-07-12 17:20:46 +0300

    as

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -6,7 +6,7 @@ function About(){
             <section className="About">
                 <p className="Description">
                     I'm a Personal Creative Developer And Designer <br />
-                    With Over 2 Years in <div className="Script JS"></div> And 1 Year Of in <img src="./src/assets/react.svg" alt="" style={{width:'30px' , height:'30px' , margin:"0 0"}} /> And <div className="Script"></div> <br />
+                    With Over 2 Years in <div className="Script JS"></div> And 1 Year Of in <img src="src/assets/react.svg" alt="" style={{width:'30px' , height:'30px' , margin:"0 0"}} /> And <div className="Script"></div> <br />
                     With Also 3 Years In CSS AND HTML Designs <br /><br />
                     When I Code Always Use React.js As A <span className="Script JS"></span> And Some Projects With <span className="Script"></span> <br />
                     And Some Time Use <span className="Rovelink">Vanila JS</span> But Not Always Use Vanila And IN UX DEsign<br />
